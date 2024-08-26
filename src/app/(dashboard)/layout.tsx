57a155186fd15f411445039359cd213294577e7a
Manish Nayal <msnayal.55@gmail.com>
2024-08-26 13:57:17 +0530

    new side bar added

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,5 @@
-import Menu from '@/components/Menu'
 import Navbar from '@/components/Navbar'
-import Image from 'next/image'
-import Link from 'next/link'
+import Sidebar from '@/components/Sidebar'
 
 export default function DashboardLayout({
   children,
@@ -11,7 +9,7 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      {/* <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
@@ -25,9 +23,12 @@ export default function DashboardLayout({
           <span className="hidden lg:block font-bold">SchoolDevN</span>
         </Link>
         <Menu />
-      </div>
+      </div> */}
+
+      <Sidebar />
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll flex flex-col">
+      {/* <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4"> */}
+      <div className="bg-[#f7f8fa] overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
